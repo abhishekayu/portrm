266e7108a470db2b6947694f689691231c5bfe9b
abhishekayu <imdarkcoder@gmail.com>
2026-04-04 21:13:38 +0530

    v1.0.5: bump all versions

--- Formula/portrm.rb
+++ Formula/portrm.rb
@@ -1,21 +1,21 @@
 class Portrm < Formula
   desc "portrm - Fast, cross-platform CLI to inspect ports, understand processes, and recover broken dev environments"
   homepage "https://portrm.dev"
-  version "1.0.4"
+  version "1.0.5"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/abhishekayu/portrm/releases/download/v1.0.4/portrm-darwin-arm64.tar.gz"
+      url "https://github.com/abhishekayu/portrm/releases/download/v1.0.5/portrm-darwin-arm64.tar.gz"
       sha256 "15de0084e26066cb037d76edff191930992cb8fc487ac7d780ce9aabcd4aa6a5"
     else
-      url "https://github.com/abhishekayu/portrm/releases/download/v1.0.4/portrm-darwin-amd64.tar.gz"
+      url "https://github.com/abhishekayu/portrm/releases/download/v1.0.5/portrm-darwin-amd64.tar.gz"
       sha256 "23f58b742175c8a6458d807943c794cc3cbf9e93474167790ad748366a78bbe3"
     end
   end
 
   on_linux do
-    url "https://github.com/abhishekayu/portrm/releases/download/v1.0.4/portrm-linux-amd64.tar.gz"
+    url "https://github.com/abhishekayu/portrm/releases/download/v1.0.5/portrm-linux-amd64.tar.gz"
     sha256 "3e9ab419a616d6a05424c1b6a8cf9a09069c50afcac288307a5bd4064f00d40a"
   end
 
